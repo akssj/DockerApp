6892a0ba15f38a25cfe4d10eda1e42558e79e920
TotalyActiveGitUster <radek.czyzowicz@gmail.com>
2024-01-27 11:45:44 +0100

    changing table creation query

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function initializeApp() {
 function startServer() {
 
   pool.query(`CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
